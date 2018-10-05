fbde4d18b3cf24767812c73a88d66982b52ca6ea
Iago Cavalcante <iagoangelimc@gmail.com>
2018-10-05 07:50:02 -0300

    Change router and Import footer in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <nav class="container" id="nav">
+    <nav class="container navbar" id="nav">
       <div class="row">
         <div class="col-md-6">
           <a href="http://">Brand</a>
         </div>
         <div class="col-md-6">
-          <div class="navbar">
+          <div class="menu">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link> |
             <router-link to="login">Login</router-link>
@@ -15,24 +15,32 @@
       </div>
     </nav>
     <router-view/>
-    <footer>
-      <div class="container">
-        Businessense
-      </div>
-    </footer>
+    <b-footer></b-footer>
   </div>
 </template>
+
+<script>
+import BFooter from './components/BFooter'
+
+export default {
+  name: 'App',
+  components: {
+    BFooter
+  }
+}
+</script>
 
 <style>
 @import url('assets/css/bootstrap.min.css');
 @import url('assets/css/bootstrap-theme.min.css');
-.navbar {
+
+.menu {
   text-align: center;
 }
-footer {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
+
+.navbar {
+  margin: 0 auto;
+  height: 85px;
+  padding-top: 30px;
 }
 </style>
