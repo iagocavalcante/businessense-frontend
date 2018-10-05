eb54773ad201c17aa51e0bf373e47b3953bec194
Iago Cavalcante <iagoangelimc@gmail.com>
2018-10-05 07:49:01 -0300

    Create footer component

---
+++ src/components/BFooter.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <section class="col-md-3">
+      <h2>SERVICES</h2>
+      <ul>
+        <li>
+          <a href="">TERTR</a>
+        </li>
+        <li>
+          <a href="">asdfasdf</a>
+        </li>
+        <li>
+          <a href="">asdfasfdfa</a>
+        </li>
+        <li>
+          <a href="">asdfasdafsd</a>
+        </li>
+        <li>
+          <a href="">asdfasdfasd</a>
+        </li>
+      </ul>
+    </section>
+    <section class="col-md-3">
+      <h2>INSTITUTIONAL</h2>
+      <ul>
+        <li>
+          <a href="">TERTR</a>
+        </li>
+        <li>
+          <a href="">asdfasdf</a>
+        </li>
+        <li>
+          <a href="">asdfasfdfa</a>
+        </li>
+        <li>
+          <a href="">asdfasdafsd</a>
+        </li>
+      </ul>
+    </section>
+    <section class="col-md-3">
+      <h2>SOCIAL MEDIA</h2>
+      <a href="">
+        <img src="" alt="">
+      </a>
+      <a href="">
+        <img src="" alt="">
+      </a>
+      <a href="">
+        <img src="" alt="">
+      </a>
+    </section>
+    <section class="col-md-3">
+      <h2>DOWNLOADS</h2>
+    </section>
+    <footer>
+      <div class="container">
+        Businessense
+      </div>
+    </footer>
+  </div>  
+</template>
+
+<style>
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
