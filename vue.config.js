e746f7f2b94518c487c992af3f254660be0648e2
Iago Cavalcante <iagoangelimc@gmail.com>
2018-10-09 00:55:11 -0300

    PWA config and image loader config

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 10240 }))
+  },
+  pwa: {
+    themeColor: '#3E413F',
+    name: 'Businessense'
+  }
+}
