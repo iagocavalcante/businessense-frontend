c4d0f0b292d5e32142ec39b5ebce29a466065a32
iago.cavalcante <IAGO.CAVALCANTE@dti-40.sefa.pa.gov.br>
2018-10-10 12:58:21 -0300

    Css utilities classes and login page modifications

--- src/App.vue
+++ src/App.vue
@@ -32,4 +32,47 @@ main {
 header, main, footer {
   flex-shrink: 0;
 }
+
+.pt-30 {
+  padding-top: 30px;
+}
+
+.pt-20 {
+  padding-top: 20px;
+}
+
+.pt-10 {
+  padding-top: 10px;
+}
+
+.pt-5 {
+  padding-top: 5px;
+}
+
+.mt-20 {
+  margin-top: 20px;
+}
+
+.mt-10 {
+  margin-top: 10px;
+}
+
+.mt-5 {
+  margin-top: 5px;
+}
+
+.mb-20 {
+  margin-bottom: 20px;
+}
+
+.mb-10 {
+  margin-bottom: 10px;
+}
+
+.mb-5 {
+  margin-bottom: 5px;
+}
+
+
+
 </style>
--- src/domains/auth/components/Login.vue
+++ src/domains/auth/components/Login.vue
@@ -1,26 +1,68 @@
 <template>
   <div class="login col-md-12 col-xs-12">
     <div class="container">
-      <div class="login-form background-login">
-        <h5 class="welcome">Welcome back, friend</h5>
-        <div class="form-group relative">
-          <input class="text-input" type="text" name="login" id="login" placeholder="EMAIL ADRESS OR LOGIN">
-          <i class="fa fa-user"></i>
+      <div class="row">
+        <div class="col-md-5 col-xs-5">
+          <div class="login-form">
+            <h5 class="welcome">Welcome back, friend</h5>
+            <div class="form-group relative">
+              <input class="text-input" type="text" name="login" id="login" placeholder="EMAIL ADRESS OR LOGIN">
+              <i class="fa fa-user"></i>
+            </div>
+            <div class="form-group relative">
+              <input class="text-input" type="password" name="password" id="password" placeholder="PASSWORD">
+              <i class="fa fa-lock"></i>
+            </div>
+            <div class="forgot-password">
+              <a class="forgot-password" href="http://">Forgot your password?</a>
+            </div>
+            <div class="form-group pt-10">
+              <button class="btn-block btn-login" @click.prevent="">Log in</button>
+            </div>
+            <div class="login-users pt-30">
+              <img src="./../../../assets/img/login-users.png" alt="">
+            </div>
+          </div>
         </div>
-        <div class="form-group relative">
-          <input class="text-input" type="password" name="password" id="password" placeholder="PASSWORD">
-          <i class="fa fa-lock"></i>
-        </div>
-        <div class="forgot-password">
-          <a class="forgot-password" href="http://">Forgot your password?</a>
-        </div>
-        <div class="form-group">
-          <button class="btn-block btn-login" @click.prevent="">Log in</button>
-        </div>
-        <p class="divider">or</p>
-        <button class="btn-block btn-facebook" @click.prevent="">Log in with Facebook</button>
-        <div class="login-users">
-          <img src="./../../../assets/img/login-users.png" alt="">
+
+        <div class="col-md-1 col-xs-1 divider-right"></div>
+
+        <div class="col-md-6 col-xs-6">
+          <div class="create-form">
+            <h5 class="welcome">You are new here?</h5>
+            <div class="form-group relative">
+              <input class="text-input" type="text" name="email" id="email" placeholder="EMAIL ADRESS OR LOGIN">
+            </div>
+            <div class="form-group relative">
+              <input class="text-input" type="text" name="firstName" id="firstName" placeholder="FIRST NAME">
+            </div>
+            <div class="form-group relative">
+              <input class="text-input" type="text" name="lastName" id="lastName" placeholder="LAST NAME">
+            </div>
+            <div class="form-group relative">
+              <input class="text-input" type="password" name="createPassword" id="createPassword" placeholder="PASSWORD">
+              <i class="fa fa-lock"></i>
+            </div>
+            <div class="form-group relative">
+              <input class="text-input" type="password" name="confirmPassword" id="confirmPassword" placeholder="CONFIRM PASSWORD">
+              <i class="fa fa-lock"></i>
+            </div>
+            <div class="col-md-12">
+              <input type="checkbox" class="form-checkbox" name="agree" id="">
+              <span class="checkbox-text">
+                I agree to the terms of use of busissense.
+              </span>
+            </div>
+            <div class="col-md-12 mb-10">
+              <input type="checkbox" class="form-checkbox" name="" id="">
+              <span class="checkbox-text">
+                I agree to receive email newsletters and contriubutions about my account.
+              </span>
+            </div>
+            <div class="form-group">
+              <button class="btn-block btn-login" @click.prevent="">Create</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -34,21 +76,24 @@ h5 {
 }
 
 .login {
+  display: block;
+  position: relative;
   width: 100%;
   height: 100%;
   background: url('./../../../assets/img/login.jpg') center center no-repeat;
+  background-position: center top;
   background-size: cover;
 }
 
-.login::before {
+.login:before {
   content: '';
+  display: block;
   position: absolute;
   top: 0;
-  right: 0;
-  bottom: 0;
   left: 0;
-  background-image: linear-gradient(to right, #65DFBE, transparent);
-  opacity: .9;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(90deg, rgba(101, 223, 190, 1) 15%, rgba(101, 223, 190, 1) 70%, rgba(250, 250, 250, 0.1) 150%);
 }
 
 .welcome {
@@ -57,9 +102,7 @@ h5 {
 }
 
 .login-users {
-  position: absolute;
-  top: 60px;
-  left: 410px;
+  text-align: center;
 }
 
 .login-users > img {
@@ -68,7 +111,6 @@ h5 {
 }
 
 .forgot-password {
-  padding-bottom: 5px;
   color: #fff;
   text-align: center;
   display: flex;
@@ -78,11 +120,17 @@ h5 {
 }
 
 .login-form {
-  position: relative;
   width: 270px;
+  position: relative;
   top: 0;
   left: 0;
   padding-top: 135px;
+  padding-bottom: 135px;
+}
+
+.create-form {
+  width: 270px;
+  padding-top: 125px;
   padding-bottom: 135px;
 }
 
@@ -92,6 +140,13 @@ h5 {
 }
 
 .login-form i {
+  position: absolute;
+  top: 14px;
+  left: 15px;
+  color:#1ED9BA;
+}
+
+.create-form i {
   position: absolute;
   top: 14px;
   left: 15px;
@@ -112,6 +167,62 @@ h5 {
   padding-left: 35px;
   outline: 0;
   color: #A39C92;
+}
+
+.form-checkbox {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 12px;
+  width: 12px;
+  margin-top: 4px;
+  cursor: pointer;
+  background: rgba(40,40,40,0.1);
+  color:black;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border: 1.4px solid #ffffff;
+}
+
+.form-checkbox:checked {
+  background: rgba(40,40,40,0.7);
+}
+
+.form-checkbox:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+.form-checkbox:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the form-checkbox when checked */
+input:checked ~ .form-checkbox:after {
+  display: block;
+}
+
+.checkbox-text {
+  color: #fff;
+  padding-left: 10px;
+  padding-top: 0;
+}
+
+.divider-right {
+  border-left: 2px solid #fff;
+  margin-top: 110px;
+  padding-right: 40px;
+  height: 270px;
 }
 
 .text-input:focus {
