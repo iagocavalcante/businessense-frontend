843eeb8173994c899b64d47579e15f9551ff9c02
Iago Cavalcante <iagoangelimc@gmail.com>
2018-10-24 23:56:35 -0300

    WIP List insights

--- src/domains/dashboard/components/Insights.vue
+++ src/domains/dashboard/components/Insights.vue
@@ -5,50 +5,86 @@
         <div class="col-md-3 col-xs-3">
           <h1>Tell more to us about your insights</h1>
           <hr class="about">
+          <div class="col-md-3">
+            <div class="new-block" style="background-color:#669999;">
+              Quadrados
+            </div>
+          </div>
+          <div>
+            <p>Quadrados</p>
+          </div>
         </div>
         <div class="col-md-9 col-xs-9">
           <div class="header-insights">
             <p class="pain-points">your pain points</p>
           </div>
           <hr class="about">
-          <div class="col-md-3 center">
-            <div class="blocks">
-              Quadrados
+          <div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color:#669999;">
+                Decreasing
+                profits
+                <div class="buttons">
+                  <a href="" class="btn">+</a>
+                  <a href="" class="btn">-</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #66CCCC;">
+                Quadrados
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #66CCCC;">
+                Quadrados
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #66CCCC;">
+                Quadrados
+                <div class="buttons">
+                  <a href="" class="btn">+</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #669999;">
+                Quadrados
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #333333;">
+                Quadrados
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #999999;">
+                Quadrados
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="blocks" style="background-color: #66CCCC;">
+                Quadrados
+              </div>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="blocks">
-              Quadrados
+          <div class="col-md-12 issues">
+            <p>issues connected to your Pain Points</p>
+            <hr class="about">
+            <div>
+              <div class="col-md-9 list-pain-points">
+                <ul>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                  <li>Testes</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -77,14 +113,57 @@ export default {
 }
 
 .blocks {
-  margin-top: 10px;
+  margin-top: 20px;
   height: 90px;
-  width: 120px;
-  border: 1px solid black;
+  width: 180px;
+  border-radius: 4px;
+  padding: 10px;
 }
 
-.center {
-  text-align: center;
+.new-block {
+  margin-top: 20px;
+  height: 90px;
+  width: 120px;
+  border-radius: 4px;
+  padding: 10px;
 }
+
+.box-center {
+  display: flex;
+  align-content: flex-start;
+}
+
+.buttons {
+  position: absolute;
+  bottom: 0;
+  right: 30px;
+}
+
+.btn {
+  padding: 5px;
+  font-size: 18px;
+}
+
+.issues {
+  padding-top: 10px;
+}
+
+.list-pain-points {
+  height: 350px;
+  display: flex;
+}
+
+.list-pain-points > ul {
+  list-style: none;
+}
+
+.list-pain-points > ul > li {
+  height: 40px;
+  border: 1px solid cadetblue;
+  margin: 5px;
+  align-items: center;
+  align-content: center;
+}
+
 </style>
 
