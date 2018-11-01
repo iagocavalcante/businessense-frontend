16d405fbf5fc9b71bafd85a054f5741d1e57b1de
Iago Cavalcante <iagoangelimc@gmail.com>
2018-11-01 15:51:32 -0300

    Risk component

---
+++ src/domains/dashboard/components/Risk.vue
@@ -0,0 +1,255 @@
+<template>
+  <div>
+    <div class="col-md-12 col-xs-12"> 
+      <div class="row">
+        <div class="page-title">
+          <div style="text-align: start;" class="container">
+            <a href=""><span><i style="margin-right: 5px;" class="arrow-new fas fa-chevron-left"></i></span>back to dashboard</a>
+          </div>
+          <h1>Innefective fixes cost budge</h1>
+          <div style="text-align: end;" class="container">
+            <p href="">Accuracy: High</p>
+            <p href="">Risk: medium</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
+        <div class="row">
+          <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
+            <div class="col-md-6 text">
+              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
+            </div>
+          </div>
+          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+          </div>
+        </div>
+      </div>
+      <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
+        <div class="row">
+          <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
+            <div class="col-md-6 text">
+              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
+            </div>
+          </div>
+          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+          </div>
+        </div>
+      </div>
+      <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
+        <div class="row">
+          <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
+            <div class="col-md-6 text">
+              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
+            </div>
+          </div>
+          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
+            <div>
+              <div class="col-md-2 margin-blocks">
+                <div class="blocks" style="background-color:#669999;">
+                  Decreasing
+                  profits
+                  <div class="buttons">
+                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
+                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2 margin-blocks">
+                <div class="blocks" style="background-color:#669999;">
+                  Decreasing
+                  profits
+                  <div class="buttons">
+                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
+                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2 margin-blocks">
+                <div class="blocks" style="background-color:#669999;">
+                  Decreasing
+                  profits
+                  <div class="buttons">
+                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
+                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2 margin-blocks">
+                <div class="blocks" style="background-color:#669999;">
+                  Decreasing
+                  profits
+                  <div class="buttons">
+                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
+                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
+        <div class="row">
+          <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
+            <div class="col-md-6 text">
+              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
+            </div>
+          </div>
+          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Risks'
+}
+</script>
+
+<style scoped>
+.header-pain-point {
+  height: 140px;
+  padding-top: 115px;
+}
+
+.page-title {
+  background-color: #66cccc;
+  height: auto;
+  text-align: center;
+}
+
+.page-title > h1 {
+  margin: 0;
+}
+
+.header-insights {
+  height: 20px;
+  margin-top: 20px;
+}
+
+.arrow-new {
+  margin-top: 20px;
+}
+
+.pain-points {
+  display: inline;
+  color: #66cccc;
+}
+
+.about {
+  border-bottom: 1.3px solid black;
+}
+
+.blocks {
+  margin-top: 10px;
+  height: 90px;
+  width: 120px;
+  border-radius: 4px;
+  padding: 10px;
+}
+
+.text {
+  color: #66cccc;
+}
+
+.new-block {
+  color: #fff;
+  margin-top: 10px;
+  height: 90px;
+  width: 120px;
+  border-radius: 4px;
+  padding: 10px;
+}
+
+.margin-blocks {
+  margin-right: 60px;
+}
+
+.new-button {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+}
+
+.box-center {
+  display: flex;
+  align-content: flex-start;
+}
+
+.buttons {
+  position: absolute;
+  bottom: 0;
+  right: -16px;
+}
+
+.btn {
+  padding: 0.15em;
+  font-size: 18px;
+  color: #fff;
+}
+
+.pull-right {
+  text-align: right;
+}
+
+.issues {
+  padding-top: 10px;
+}
+
+.list-pain-points {
+  height: 350px;
+  width: 100%;
+  display: flex;
+  overflow: auto;
+}
+
+.list-pain-points > ul {
+  padding: 0;
+  list-style: none;
+  flex: 1;
+}
+
+.list-pain-points > ul > li {
+  height: 40px;
+  background-color: #1f4238;
+  margin: 5px;
+  align-items: center;
+  align-content: center;
+  border-radius: 5px;
+  padding: 10px;
+  color: #fff;
+}
+
+.button-padding {
+  padding-bottom: 20px;
+}
+
+.btn-next-step {
+  height: 40px;
+  border-radius: 3px;
+  border: 0;
+  background-color: #323031;
+  color: #66cccc;
+  margin-top: 10px;
+}
+
+.btn-next-step:active {
+  height: 40px;
+  border-radius: 3px;
+  border: 0;
+  background-color: #323031;
+  color: #66cccc;
+}
+</style>
+
