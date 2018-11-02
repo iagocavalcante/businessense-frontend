80c0e35716e2d3d5369109efe46d11f08aee35cd
Iago Cavalcante <iagoangelimc@gmail.com>
2018-11-02 19:43:52 -0300

    Finish risks components

--- src/domains/dashboard/components/Risk.vue
+++ src/domains/dashboard/components/Risk.vue
@@ -4,90 +4,81 @@
       <div class="row">
         <div class="page-title">
           <div style="text-align: start;" class="container">
-            <a href=""><span><i style="margin-right: 5px;" class="arrow-new fas fa-chevron-left"></i></span>back to dashboard</a>
+            <a class="a-page-title" href="">
+              <span>
+                <i style="margin-right: 5px;" class="arrow-new fas fa-chevron-left"></i>
+              </span>
+              Back to dashboard
+            </a>
           </div>
           <h1>Innefective fixes cost budge</h1>
           <div style="text-align: end;" class="container">
-            <p href="">Accuracy: High</p>
-            <p href="">Risk: medium</p>
+            <p href="">Accuracy: <span style="color: #669999">High</span></p>
+            <p href="">Risk: <span style="color: #669999">medium</span></p>
           </div>
         </div>
       </div>
     </div>
-    <div class="container">
-      <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
+    <div class="container" style="padding-top: 30px;">
+      <div style="border-bottom: 1px solid black; margint-top: 30px;" class="col-md-12 col-xs-12"> 
         <div class="row">
           <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
-            <div class="col-md-6 text">
-              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
+            <div class="col-md-2">
+              <span><i class="orange-icon mt-20 far fa-5x fa-file-alt"></i></span>
+              <h1 class="risks-h1">Description</h1>
             </div>
           </div>
-          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+          <div style="padding: 10px;" class="col-md-8 col-xs-8">
+            <p class="sugestions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+            <p class="sugestions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
           </div>
         </div>
       </div>
       <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
         <div class="row">
           <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
-            <div class="col-md-6 text">
-              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
+            <div class="col-md-2">
+              <span><i class="orange-icon mt-20 fas fa-5x fa-chart-line"></i></span>
+              <h1 class="risks-h1">Stats</h1>
             </div>
           </div>
-          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+          <div style="padding: 10px;" class="col-md-8 col-xs-8">
+            <p class="sugestions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+            <p class="sugestions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
           </div>
         </div>
       </div>
       <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
-        <div class="row">
+        <div class="row pt-20">
           <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
-            <div class="col-md-6 text">
-              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
-            </div>
+            <span><i class="orange-icon mt-20 fas fa-5x fa-map-marker-alt"></i></span>
+            <h1 class="risks-h1">Usual Pain
+              <br>Points</h1>
           </div>
           <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
             <div>
-              <div class="col-md-2 margin-blocks">
-                <div class="blocks" style="background-color:#669999;">
+              <div class="col-md-3">
+                <div class="blocks medium">
                   Decreasing
                   profits
-                  <div class="buttons">
-                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
-                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
-                  </div>
                 </div>
               </div>
-              <div class="col-md-2 margin-blocks">
-                <div class="blocks" style="background-color:#669999;">
+              <div class="col-md-3">
+                <div class="blocks high">
                   Decreasing
                   profits
-                  <div class="buttons">
-                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
-                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
-                  </div>
                 </div>
               </div>
-              <div class="col-md-2 margin-blocks">
-                <div class="blocks" style="background-color:#669999;">
+              <div class="col-md-3">
+                <div class="blocks low">
                   Decreasing
                   profits
-                  <div class="buttons">
-                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
-                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
-                  </div>
                 </div>
               </div>
-              <div class="col-md-2 margin-blocks">
-                <div class="blocks" style="background-color:#669999;">
+              <div class="col-md-3">
+                <div class="blocks low">
                   Decreasing
                   profits
-                  <div class="buttons">
-                    <a href="" class="btn"><i class="fas fa-plus-circle"></i></a>
-                    <a href="" class="btn"><i class="fas fa-minus-circle"></i></a>
-                  </div>
                 </div>
               </div>
             </div>
@@ -97,13 +88,11 @@
       <div style="border-bottom: 1px solid black;" class="col-md-12 col-xs-12"> 
         <div class="row">
           <div style="padding-bottom: 10px; margin-right: 10px;" class="col-md-3 col-xs-3">
-            <div class="col-md-6 text">
-              <span><i class="arrow-new fas fa-5x fa-chevron-right"></i></span>
-            </div>
+            <h1 class="risks-h1">Risks Factors</h1>
           </div>
-          <div style="padding-bottom: 10px;" class="col-md-8 col-xs-8">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+          <div style="padding: 10px;" class="col-md-8 col-xs-8">
+            <p class="sugestions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
+            <p class="sugestions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta repudiandae sint alias inventore ex similique rem, dignissimos minima recusandae, quae eaque cum nesciunt nobis repellat cumque asperiores vel hic?</p>
           </div>
         </div>
       </div>
@@ -124,18 +113,30 @@ export default {
 }
 
 .page-title {
+  color: #fff;
+  font-family: CircularStd-Book;
   background-color: #66cccc;
   height: auto;
   text-align: center;
 }
 
+.a-page-title {
+  color: #fff;
+}
+
 .page-title > h1 {
+  font-weight: 100;
   margin: 0;
 }
 
 .header-insights {
   height: 20px;
   margin-top: 20px;
+}
+
+.sugestions {
+  color: #333333;
+  font-family: Circularstd-Book;
 }
 
 .arrow-new {
@@ -154,26 +155,13 @@ export default {
 .blocks {
   margin-top: 10px;
   height: 90px;
-  width: 120px;
   border-radius: 4px;
   padding: 10px;
+  color: #fff;
 }
 
 .text {
   color: #66cccc;
-}
-
-.new-block {
-  color: #fff;
-  margin-top: 10px;
-  height: 90px;
-  width: 120px;
-  border-radius: 4px;
-  padding: 10px;
-}
-
-.margin-blocks {
-  margin-right: 60px;
 }
 
 .new-button {
@@ -250,6 +238,26 @@ export default {
   border: 0;
   background-color: #323031;
   color: #66cccc;
+}
+
+.high {
+ background-color: #FF9933;
+}
+
+.medium {
+ background-color: #663300;
+}
+
+.low {
+  background-color: #996633;
+}
+
+.risks-h1 {
+  font-family: Circularstd-Book;
+}
+
+.orange-icon {
+  color: #FF9933;
 }
 </style>
 
